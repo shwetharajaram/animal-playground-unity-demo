d90e37e899854b4687e6e76d15a48f2ca1fc35cc
Shwetha Rajaram <shwethar@umich.edu>
2021-09-24 17:02:42 -0400

    starter project for SI 559 workshop

---
+++ Assets/Scripts/EnvironmentToggle.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnvironmentToggle : MonoBehaviour
+{
+    //public Material earthSkybox, moonSkybox, marsSkybox;
+    public GameObject earthEnvironment, moonEnvironment, marsEnvironment;
+    public GameObject[] spaceHelmets;
+
+    public void ShowEarth()
+    {
+        Debug.Log("Showing Earth environment");
+
+        //RenderSettings.skybox = earthSkybox;
+
+        earthEnvironment.SetActive(true);
+        moonEnvironment.SetActive(false);
+        marsEnvironment.SetActive(false);
+
+        Physics.gravity = new Vector3(0, -9.807f, 0);
+
+        foreach (GameObject go in spaceHelmets) {
+            go.SetActive(false);
+        }
+    }
+
+    public void ShowMoon()
+    {
+        Debug.Log("Showing Moon environment");
+       
+        //RenderSettings.skybox = moonSkybox;
+
+        moonEnvironment.SetActive(true);
+        earthEnvironment.SetActive(false);
+        marsEnvironment.SetActive(false);
+
+        Physics.gravity = new Vector3(0, -1.62f, 0);
+
+        foreach (GameObject go in spaceHelmets)
+        {
+            go.SetActive(true);
+        }
+    }
+
+    public void ShowMars()
+    {
+        Debug.Log("Showing Mars environment");
+
+        //RenderSettings.skybox = marsSkybox;
+
+        marsEnvironment.SetActive(true);
+        moonEnvironment.SetActive(false);
+        earthEnvironment.SetActive(false);
+
+        Physics.gravity = new Vector3(0, -3.711f, 0);
+
+        foreach (GameObject go in spaceHelmets)
+        {
+            go.SetActive(true);
+        }
+    }
+}
---
+++ Assets/Scripts/RotateRoundabout.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateRoundabout : MonoBehaviour
+{
+    public float rotationAngle; // y-rotation
+    private bool rotateObject = true;
+
+    public GameObject roundaboutFrame;
+    private Dictionary<int, GameObject> animalsOnRoundabout;
+
+    private void Start()
+    {
+        animalsOnRoundabout = new Dictionary<int, GameObject>();
+    }
+
+    private void Update() {
+        if (Input.GetKeyDown(KeyCode.R)) {
+            Debug.Log("R pressed");
+            rotateObject = !rotateObject;
+        }
+
+        if (rotateObject) {  
+            transform.Rotate(0, rotationAngle, 0, Space.Self);
+
+            foreach (GameObject animal in animalsOnRoundabout.Values)
+            {
+                animal.transform.RotateAround(this.transform.position, Vector3.up, rotationAngle);
+            }
+        }
+    }
+
+    private void OnTriggerEnter(Collider collider)
+    {
+        Debug.Log("animal on roundabout!");
+        int instanceID = collider.gameObject.GetInstanceID();
+        if (!animalsOnRoundabout.ContainsKey(instanceID))
+        {
+            animalsOnRoundabout.Add(instanceID, collider.gameObject);
+        }
+    }
+
+    private void OnTriggerExit(Collider collider)
+    {
+        Debug.Log("animal off roundabout");
+        int instanceID = collider.gameObject.GetInstanceID();
+        if (animalsOnRoundabout.ContainsKey(instanceID))
+        {
+            animalsOnRoundabout.Remove(instanceID);
+        }
+    }
+}
